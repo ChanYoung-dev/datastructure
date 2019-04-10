653f91bc7a1bc99b4689f821590abbdf30ad31b1
kimchanyoung <emrhs1004@naver.com>
2019-04-10 21:54:59 +0900

    구조체를 사용하여 최고점수를 갖은 학생 최소점수를 갖은 학생 출력

--- main.c
+++ main.c
@@ -1,61 +1,71 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define MAX_STACK_SIZE 100
-typedef char Element;
-Element data[MAX_STACK_SIZE];
-int top; // 스택이 현재 가리키는 부분
-void error(char str[])
-{
-    printf("%s\n", str);
-    exit(1);
-}
-void init_stack(){top=-1;}
-int is_empty(){return top==-1;}
-int is_full(){return top==MAX_STACK_SIZE-1;}
-int size(){return top+1;}
-void push(Element e)
-{
-    if(is_full())
-        error("스택 포화 에러");
-    data[++top]=e;
-}
-Element pop(){
-    if(is_empty())
-        error("스택 공백 에러");
-    return data[top--];
-}
-Element peek()
-{
-    if(is_empty())
-        error("스택 공백 에러");
-    return data[top];
-}
-//스택을 쌓고 그대로 추출해준다.(push를 한 후 pop)
-void push_pop(char expr[])
-{
-    int i=0; //문자열의 현재 index
-    char ch,prev; // ch는 글자를 저장할 변수형,prev는 스택에서 추출한 글자를 저장할 변수형
-    init_stack(); //스택 초기화
-    while(expr[i]!='\0'){ // 문자열의 끝부분 '/0'을 도착하기전까지 반복
-        ch=expr[i++]; //문자열의 i-1번째 문자를 ch에 입력
-        push(ch);  //스택에 ch를 삽입
-    }
-    // 스택이 비워질때까지 pop을 해주고 출력해준다.
-    while(is_empty()!=1){ //스택이 비워질때까지 반복한다.
-        prev=pop();  //스택을 pop해주고 prev변수에 삽입
-        printf("%c",prev); //추출한 스택내용을 출력
+#include <memory.h>
+// 학생의 구조체
+typedef struct Student_t{
+    int id;
+    char name[20];
+    double score;
+}Student;
+//학생 입력
+void insertStudent(Student st[]){
+    for(int i=0;i<10;i++)
+    {
+        printf("%d번째 학생이름입력:",i+1);
+        scanf("%s",&st[i].name);
+        printf("%d번째 학생학번입력:",i+1);
+        scanf("%d",&(st[i].id));
+        printf("%d번째 학생성적입력:",i+1);
+        scanf("%lf",&(st[i].score));
+        printf("%s,%d, %0.2lf \n",&st[i].name,st[i].id,st[i].score);
     }
 }
-int main()
-{
+//최대값구하는 함수
+void MaxScore(Student s[10],double maxscore,char maxname[],int maxid){
+    //비교
+    for(int i=0;i<10;i++) {
+        //비교값보다 더 큰 점수를 갖은 학생 발견시
+        if (maxscore < s[i].score) {
+            maxscore = s[i].score;
+            strcpy(maxname,s[i].name);
+            maxid = s[i].id;
+        }
+    }
+    printf("%d학번 %s의 점수가 %0.2lf이므로 제일 높습니다.", maxid,maxname,maxscore);
+}
+//최소값구하는 함수
+void MinScore(Student s[10],double minscore,char minname[],int minid){
+    /*
+     minname은 name의 주소가 사용된다.
+     즉,
+     MaxScore(student,score,name,id);코드에서
+     MaxScore에서 사용된 maxname이 이미 main에서의 name의 주소를 사용하기때문에 변경된 name에는 maxname이 들어가있다.
+     즉,초기화를 위해 strcpy(minname,s[0].name; 를 사용해 준다.
+     */
+    strcpy(minname,s[0].name);
+    //비교
+    for(int i=0;i<10;i++) {
+        //비교값보다 더 작은 점수를 갖은 학생 발견시
+        if (minscore > s[i].score) {
+            minscore = s[i].score;
+            strcpy(minname,s[i].name);
+            minid = s[i].id;
+        }
+    }
+    printf("%d학번 %s의 점수가 %0.2lf이므로 제일 낮습니다.", minid,minname,minscore);
+}
 
-    char str[80];               // 문자열을 저장할 배열 선언
-    printf("문자열을 입력해주세요:");
-    scanf("%s",str); //문자열을 입력받는다.
-    push_pop(str); //push를 하고 난뒤 pop해주는 함수 실행
+int main() {
+    Student student[10];
+    //학생 입력
+    insertStudent(student);
+    //비교에 쓰일 초기값 학생 선언
+    double score=student[0].score;
+    char name[20];
+    strcpy(name,student[0].name);
+    int id=student[0].id;
+    //최대값최소값 구하기
+    MaxScore(student,score,name,id);
+    MinScore(student,score,name,id);
     return 0;
-
 }
 

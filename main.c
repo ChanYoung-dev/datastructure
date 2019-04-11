4d5363f625d42444fc0dc88f238a89190c135194
kimchanyoung <emrhs1004@naver.com>
2019-04-11 10:36:41 +0900

    3.4 postfix

--- main.c
+++ main.c
@@ -1,95 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#define MAX_STACK_SIZE 100      // 스택 요소 저장을 위한 배열의 크기
+typedef double Element;           // 스택 요소의 자료형 정의
 
 
-#define MAX_STACK_SIZE 100
-typedef char Element;
-Element data[MAX_STACK_SIZE];
-int top; // 스택이 현재 가리키는 부분
-void error(char str[])
+Element data[MAX_STACK_SIZE];   // 실제 스택요소의 배열
+int top;                        // 실제 스택의 top
+
+void error(char str[])          // 오류 상황 처리를 위한 함수
 {
     printf("%s\n", str);
     exit(1);
 }
-void init_stack(){top=-1;}
-int is_empty(){return top==-1;}
-int is_full(){return top==MAX_STACK_SIZE-1;}
-int size(){return top+1;}
+
+void init_stack() { top = -1; }
+int is_empty() { return top == -1; }
+int is_full() { return top == MAX_STACK_SIZE - 1; }
+int size() { return top + 1; }
+
 void push(Element e)
 {
-    if(is_full())
+    if (is_full())
         error("스택 포화 에러");
-    data[++top]=e;
+    data[++top] = e;
 }
-Element pop(){
-    if(is_empty())
+Element pop() {
+    if (is_empty())
         error("스택 공백 에러");
     return data[top--];
 }
 Element peek()
 {
-    if(is_empty())
+    if (is_empty())
         error("스택 공백 에러");
     return data[top];
 }
-//스택을 쌓고 그대로 추출해준다.(push를 한 후 pop)
-int check_matching(char filename[])
+
+double calc_postfix(char expr[])
 {
-    int i,j = 0, prev;
-    char ch;
-    char string[100];
-    FILE *fp = fopen(filename, "r");
-    for (i = 0; (i < (sizeof(string)-1) &&((ch = fgetc(fp)) != EOF) && (ch != '\n')); i++) {
-            ch = fgetc(fp);
-            if(ch==39)
-                while(ch!=39);
-            else
-                string[i] = ch;
+    char c;
+    int i=0;
+    double val,val1,val2;
 
-
-    }
-    string[i] = '\0';
-    fclose(fp);
-    printf("string?%s",string);
     init_stack();
-    while (string[j] != '\0') {
-        ch = string[j++];
-        if (ch == '[' || ch == '(' || ch == '{')
-            push(ch);
-        else if (ch == ']' || ch == ')' || ch == '}') {
-            if (is_empty())
-                return 2;    // 조건 2 위반
-            prev = pop();
-            if ((ch == ']' && prev != '[')
-                || (ch == ')' && prev != '(')
-                || (ch == '}' && prev != '{')) {
-                return 3;    // 조건 3 위반
+    while(expr[i]!='\0'){
+        c=expr[i++];
+        if(c>='0'&&c<='9'){
+            val = c -'0';
+            push(val);
+        }
+        else if(c=='+' || c=='-'|| c=='*'|| c=='/'){
+            val2=pop();
+            val1=pop();
+            switch(c){
+                case '+': push(val1+val2);break;
+                case '-': push(val1-val2);break;
+                case '*': push(val1*val2);break;
+                case '/': push(val1/val2);break;
             }
         }
-
     }
-    if (is_empty() == 0) return 1;        // 조건 1 위반
-    return 0;                // 괄호 정상
+    return pop();
 }
 
-int main()
+void main()
 {
+    char expr[2][80]={"8 2 / 3- 3 2 * +", "1 2 / 4 * 1 4 / *"};
 
-
-
-    int errCode, i;
-
-
-    errCode = check_matching("asdf.txt");
-    if (errCode == 0)
-        printf(" 정상 ");
-    else
-        printf(" 오류:  (조건%d 위반)\n", errCode);
-
-
-
-    return 0;
+    printf("수식: %s = %lf\n",expr[0],calc_postfix(expr[0]));
+    printf("수식: %s = %lf\n",expr[1],calc_postfix(expr[1]));
 }
-
 

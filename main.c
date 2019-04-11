7bfa31b02221d6a9d721dee2d9c39404814e93dc
kimchanyoung <emrhs1004@naver.com>
2019-04-11 10:04:53 +0900

    homework-프로그래밍프로젝트3장

--- main.c
+++ main.c
@@ -1,71 +1,95 @@
 #include <stdio.h>
-#include <memory.h>
-// 학생의 구조체
-typedef struct Student_t{
-    int id;
-    char name[20];
-    double score;
-}Student;
-//학생 입력
-void insertStudent(Student st[]){
-    for(int i=0;i<10;i++)
-    {
-        printf("%d번째 학생이름입력:",i+1);
-        scanf("%s",&st[i].name);
-        printf("%d번째 학생학번입력:",i+1);
-        scanf("%d",&(st[i].id));
-        printf("%d번째 학생성적입력:",i+1);
-        scanf("%lf",&(st[i].score));
-        printf("%s,%d, %0.2lf \n",&st[i].name,st[i].id,st[i].score);
-    }
+#include <stdlib.h>
+#include <string.h>
+
+
+#define MAX_STACK_SIZE 100
+typedef char Element;
+Element data[MAX_STACK_SIZE];
+int top; // 스택이 현재 가리키는 부분
+void error(char str[])
+{
+    printf("%s\n", str);
+    exit(1);
 }
-//최대값구하는 함수
-void MaxScore(Student s[10],double maxscore,char maxname[],int maxid){
-    //비교
-    for(int i=0;i<10;i++) {
-        //비교값보다 더 큰 점수를 갖은 학생 발견시
-        if (maxscore < s[i].score) {
-            maxscore = s[i].score;
-            strcpy(maxname,s[i].name);
-            maxid = s[i].id;
-        }
-    }
-    printf("%d학번 %s의 점수가 %0.2lf이므로 제일 높습니다.", maxid,maxname,maxscore);
+void init_stack(){top=-1;}
+int is_empty(){return top==-1;}
+int is_full(){return top==MAX_STACK_SIZE-1;}
+int size(){return top+1;}
+void push(Element e)
+{
+    if(is_full())
+        error("스택 포화 에러");
+    data[++top]=e;
 }
-//최소값구하는 함수
-void MinScore(Student s[10],double minscore,char minname[],int minid){
-    /*
-     minname은 name의 주소가 사용된다.
-     즉,
-     MaxScore(student,score,name,id);코드에서
-     MaxScore에서 사용된 maxname이 이미 main에서의 name의 주소를 사용하기때문에 변경된 name에는 maxname이 들어가있다.
-     즉,초기화를 위해 strcpy(minname,s[0].name; 를 사용해 준다.
-     */
-    strcpy(minname,s[0].name);
-    //비교
-    for(int i=0;i<10;i++) {
-        //비교값보다 더 작은 점수를 갖은 학생 발견시
-        if (minscore > s[i].score) {
-            minscore = s[i].score;
-            strcpy(minname,s[i].name);
-            minid = s[i].id;
-        }
+Element pop(){
+    if(is_empty())
+        error("스택 공백 에러");
+    return data[top--];
+}
+Element peek()
+{
+    if(is_empty())
+        error("스택 공백 에러");
+    return data[top];
+}
+//스택을 쌓고 그대로 추출해준다.(push를 한 후 pop)
+int check_matching(char filename[])
+{
+    int i,j = 0, prev;
+    char ch;
+    char string[100];
+    FILE *fp = fopen(filename, "r");
+    for (i = 0; (i < (sizeof(string)-1) &&((ch = fgetc(fp)) != EOF) && (ch != '\n')); i++) {
+            ch = fgetc(fp);
+            if(ch==39)
+                while(ch!=39);
+            else
+                string[i] = ch;
+
+
     }
-    printf("%d학번 %s의 점수가 %0.2lf이므로 제일 낮습니다.", minid,minname,minscore);
+    string[i] = '\0';
+    fclose(fp);
+    printf("string?%s",string);
+    init_stack();
+    while (string[j] != '\0') {
+        ch = string[j++];
+        if (ch == '[' || ch == '(' || ch == '{')
+            push(ch);
+        else if (ch == ']' || ch == ')' || ch == '}') {
+            if (is_empty())
+                return 2;    // 조건 2 위반
+            prev = pop();
+            if ((ch == ']' && prev != '[')
+                || (ch == ')' && prev != '(')
+                || (ch == '}' && prev != '{')) {
+                return 3;    // 조건 3 위반
+            }
+        }
+
+    }
+    if (is_empty() == 0) return 1;        // 조건 1 위반
+    return 0;                // 괄호 정상
 }
 
-int main() {
-    Student student[10];
-    //학생 입력
-    insertStudent(student);
-    //비교에 쓰일 초기값 학생 선언
-    double score=student[0].score;
-    char name[20];
-    strcpy(name,student[0].name);
-    int id=student[0].id;
-    //최대값최소값 구하기
-    MaxScore(student,score,name,id);
-    MinScore(student,score,name,id);
+int main()
+{
+
+
+
+    int errCode, i;
+
+
+    errCode = check_matching("asdf.txt");
+    if (errCode == 0)
+        printf(" 정상 ");
+    else
+        printf(" 오류:  (조건%d 위반)\n", errCode);
+
+
+
     return 0;
 }
+
 

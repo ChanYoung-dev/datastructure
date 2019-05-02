3c34d88658e771905e363f7c28bec68b7f1ba4c4
kimchanyoung <emrhs1004@naver.com>
2019-05-02 12:01:10 +0900

    ing

--- main.c
+++ main.c
@@ -1,102 +1,188 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-#define MAX_QUEUE_SIZE    100
+#define MAXLEN (int)1e3
+#define STACKSIZE (int)1e3
 
-typedef int Element;
-Element data[MAX_QUEUE_SIZE];    // 요소의 배열
-int    front;            // 전단
-int    rear;            // 후단
+#define PLUS (int)1<<30
+#define MINUS (int)(1<<30)-1
+#define MUL (int)(1<<30)-2
+#define DIV (int)(1<<30)-3
+#define OPEN (int)(1<<30)-4
+#define CLOSE (int)(1<<30)-5
 
-// 프로그램 3.1의 error()함수와 동일
-void error(char* str)
+char isNumber(char c) { return c >= '0' && c <= '9'; };
+char isMulDiv(int c) { return c == MUL || c == DIV; };
+char isOperator(int c) { return c == MUL || c == DIV || c == MINUS || c == PLUS; }
+
+double calc(char* exp)
 {
-    fprintf(stderr, "%s\n", str);
-    exit(1);
-};
-// 큐의 주요 연산: 공통
-void init_queue() { front = rear = 0; ; }
-int is_empty() { return front == rear; }
-int is_full() { return front == (rear + 1) % MAX_QUEUE_SIZE; }
-int size() { return(rear - front + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE; }
+    int len = strlen(exp);
 
-void enqueue(Element val)
-{
-    if (is_full())
-        error("  큐 포화 에러");
-    rear = (rear + 1) % MAX_QUEUE_SIZE;
-    data[rear] = val;
-}
-Element dequeue()
-{
-    if (is_empty())
-        error("  큐 공백 에러");
-    front = (front + 1) % MAX_QUEUE_SIZE;
-    return data[front];
-}
-Element peek()
-{
-    if (is_empty())
-        error("  큐 공백 에러");
-    return data[(front + 1) % MAX_QUEUE_SIZE];
-}
+    double stack[STACKSIZE];
+    int top = -1;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (exp[i] == '"') continue;
+
+        if (isNumber(exp[i]))
+        {
+            int n_number = 0;
+            double p_number = 0;
+
+            int j = i;
+            char meetPoint = 0;
+
+            while (j < len && ((exp[j] >= '0' && exp[j] <= '9') || exp[j] == '.'))
+            {
+                if (exp[j] == '.')
+                {
+                    meetPoint = 1;
+                    j++;
+                    continue;
+                }
+                if (meetPoint==0)
+                {
+                    n_number *= 10;
+                    n_number += (exp[j] - '0');
+                    j++;
+                }
+                else
+                {
+                    p_number *= 10;
+                    p_number += (exp[j] - '0');
+                    j++;
+                }
+            }
+            i = j - 1;
+
+            while (p_number > 1) p_number /= 10;
+            double now = n_number + p_number;
+
+            char MulDiv = 0;
+            if (top>=0) MulDiv = isMulDiv(stack[top]);
+
+            if (MulDiv)
+            {
+                if (stack[top] == MUL)
+                {
+                    stack[top-1] = stack[top - 1] * now;
+                }
+                else stack[top - 1] = stack[top - 1] / now;
+                top--;
+            }
+            else stack[++top] = now;
+
+        }
+        else if (exp[i] == '(') stack[++top] = OPEN;
+        else if (exp[i] == ')')
+        {
+            double tmpstack[STACKSIZE];
+            int tmptop = -1;
+
+            while (stack[top] != OPEN)
+            {
+                tmpstack[++tmptop] = stack[top];
+                top--;
+            }
+            top--;
 
 
-void init_deque() { init_queue(); }
-void add_rear(Element val) { enqueue(val); }
-Element delete_front() { return dequeue(); }
-Element get_front() { return peek(); }
+            double result = 0;
+            int oper = 1;
+            while (tmptop >= 0)
+            {
+                if (tmpstack[tmptop] == PLUS || tmpstack[tmptop] == MINUS )
+                {
+                    if (tmpstack[tmptop] == PLUS) oper = 1;
+                    else oper = -1;
+                }
+                else result += (oper)*(tmpstack[tmptop]);
+                tmptop--;
+            }
 
-void add_front(Element val)
-{
-    if (is_full())
-        error("  덱 포화 에러");
-    data[front] = val;
-    front = (front - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
-}
-
-Element delete_rear()
-{
-    int prev = rear;
-    if (is_empty())
-        error("  덱 공백 에러");
-    rear = (rear - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
-    return data[prev];
-}
-
-Element get_rear()
-{
-    if (is_empty())
-        error("  덱 공백 에러");
-    return data[rear];
-}
-
-// 큐 테스트를 위한 코드: 요소 종류마다 수정
-void print_queue(char msg[]) {
-    int i, maxi = rear;
-    if (front >= rear) maxi += MAX_QUEUE_SIZE;
-    printf("%s[%2d]= ", msg, size());
-    for (i = front + 1; i <= maxi; i++)
-        printf("%2d ", data[i%MAX_QUEUE_SIZE]);
-    printf("\n");
-}
-
-void print_deque(char msg[]) { print_queue(msg); }
-
-int main()
-{
-    int i;
-
-    init_deque();
-    for (i = 1; i<10; i++) {
-        if (i % 2) add_front(i);
-        else add_rear(i);
+            if (top >= 0 && isMulDiv(stack[top]))
+            {
+                if (stack[top] == MUL) stack[top - 1] = stack[top - 1] * result;
+                else stack[top - 1] = stack[top - 1] / result;
+                top--;
+            }
+            else stack[++top] = result;
+        }
+        else if (
+                exp[i] == '+' || exp[i] == '-' ||
+                exp[i] == '*' || exp[i] == '/')
+        {
+            if (exp[i] == '+') stack[++top] = PLUS;
+            else if (exp[i] == '-') stack[++top] = MINUS;
+            else if (exp[i] == '*') stack[++top] = MUL;
+            else stack[++top] = DIV;
+        }
+        else
+        {
+            printf("이상한 입력이 있습니다.");
+            return -1;
+        };
     }
-    print_deque("원형 덱 홀수-짝수  ");
-    printf("\tdelete_front() --> %d\n", delete_front());
-    printf("\tdelete_rear () --> %d\n", delete_rear());
-    printf("\tdelete_front() --> %d\n", delete_front());
-    print_deque("원형 덱 삭제-홀짝홀");
 
-    return 0;
+    double tmpstack[STACKSIZE];
+    int tmptop = -1;
+
+    while (top>=0 )
+    {
+        tmpstack[++tmptop] = stack[top];
+        top--;
+    }
+
+    double result = 0;
+    int oper = 1;
+    while (tmptop >= 0)
+    {
+        if (tmpstack[tmptop] == PLUS || tmpstack[tmptop] == MINUS )
+        {
+            if (tmpstack[tmptop] == PLUS) oper = 1;
+            else oper = -1;
+        }
+        else result += (oper)*(tmpstack[tmptop]);
+        tmptop--;
+    }
+    return result;
+
+}
+
+void ESpace(char *ap_st)
+{
+    char *p = ap_st;
+    while(*ap_st !=0){
+        if(*ap_st != ' '){
+            if(p!= ap_st) *p = *ap_st;
+            p++;
+        }
+        ap_st++;
+    }
+    *p = 0;
+}
+
+int main(int argc, char* argv[])
+{
+    char exp[MAXLEN];
+    memset(exp, 0, sizeof(exp));
+
+    if (argc == 1)
+    {
+        printf("%d",PLUS);
+        printf("식을 입력하세요\n");
+        printf(">> ");
+        scanf("%s", exp);
+    }
+    else if (argc == 2) strcpy(exp, argv[1]);
+    else
+    {
+        printf("하나의 식만 입력해 주세요\n");
+        return -1;
+    }
+
+    ESpace(exp);
+    printf("Result = %lf\n", calc(exp));
 }
